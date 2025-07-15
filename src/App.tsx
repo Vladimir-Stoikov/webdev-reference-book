8e580737a6903ec64121770b60463b918d27632c
Vladimir Stoikov <vspsmoke@gmail.com>
2025-07-15 21:37:33 +0300

    feat(jsData.json): add json data, import to app first step

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import data from './data/jsData.json';
 
 function App() {
+  console.log(data.lessons[0].content);
+
   return (
     <>
       <Navbar />
