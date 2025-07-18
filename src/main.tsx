95d518a98205634ebce0854fdfb7a7628876bfc8
Vladimir Stoikov <vspsmoke@gmail.com>
2025-07-18 19:54:36 +0300

    feat(App): add Not Found Page(for 404 error)

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,12 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { createBrowserRouter, RouterProvider } from 'react-router';
+import NotFoundPage from './pages/NotFoundPage.tsx';
 
-const router = createBrowserRouter([{ path: '/', element: <App /> }]);
+const router = createBrowserRouter([
+  { path: '/', element: <App /> },
+  { path: '*', element: <NotFoundPage /> },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
