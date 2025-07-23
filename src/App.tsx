ce2a2fc73aeac854a326669a2a1927d9fb2d69bb
Vladimir Stoikov <vspsmoke@gmail.com>
2025-07-23 22:13:54 +0300

    feat(App) add styles to infoCard and App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
+import styled from 'styled-components';
 import './App.css';
 import InfoCard from './components/InfoCard';
 import Navbar from './components/Navbar';
+
+const MainSt = styled.main`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 function App() {
   return (
     <>
       <Navbar />
-      <InfoCard />
+      <MainSt>
+        <InfoCard />
+      </MainSt>
     </>
   );
 }
--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
 
 import data from '../data/jsData.json';
+import styled from 'styled-components';
+
+const SectionSt = styled.section`
+  width: 30%;
+  height: 30%;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: 2rem;
+  border: solid 3px white;
+  border-radius: 20px;
+  box-shadow: 10px 10px 10px rgba(50, 50, 50, 0.5);
+  background: linear-gradient(46deg, rgba(34, 34, 34, 1) 0%, rgba(34, 34, 34, 1) 20%, #444444 52%, #3a3a3a 75%, rgba(34, 34, 34, 1) 100%);
+`;
 
 export default function InfoCard() {
   const header = data.lessons[0].header;
-  return <h3>{header}</h3>;
+  return (
+    <SectionSt>
+      <h3>{header}</h3>
+    </SectionSt>
+  );
 }
