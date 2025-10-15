a0eba116f492cba812718abf18df950f720367ef
Vladimir Stoikov <vspsmoke@gmail.com>
2025-10-15 19:58:12 +0300

    feat(useProgress.ts): add base logic and work with localStorage

--- src/hooks/useProgress.ts
+++ src/hooks/useProgress.ts
@@ -2,19 +2,27 @@ import { useCallback, useEffect, useState } from "react"
 
 export function useProgress() {
 
-  const [progress, setProgress] = useState<Record<string, unknown>>({})
+  const [progress, setProgress] = useState<Record<string, {lastRead: string}>>({})
 
   useEffect(() => {
-    // загрузка данных из localStore
-  })
+    const loadedSavedData = localStorage.getItem('readingProgress');
+    if(loadedSavedData) {
+      setProgress(JSON.parse(loadedSavedData));
+    }
+  }, [])
 
   const lastRead = useCallback((id: string) => {
-    // сохранение последней даты чтнения
-  }, [])
+    const newRecord = {
+      ...progress,
+      [id]: {lastRead: new Date().toISOString()},
+    };
+    setProgress(newRecord);
+    localStorage.setItem('readingProgress', JSON.stringify(newRecord));
+  }, [progress])
 
   const getProgress = useCallback((id: string) => {
-    // отслеживание статуса 
-  }, [])
+    return progress[id] || null;
+  }, [progress])
 
   return { progress, lastRead, getProgress};
 }
